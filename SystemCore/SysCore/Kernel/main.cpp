fbb52fa74c0aae9eabe3f066c110afd24ef665dd
ali <design0098@yahoo.com>
2018-12-26 15:53:50 -0800

    add physical memory managment to Kernel

--- SystemCore/SysCore/Kernel/main.cpp
+++ SystemCore/SysCore/Kernel/main.cpp
@@ -2,6 +2,7 @@
 #include <Hal.h>
 #include "exception.h"
 #include <bootinfo.h>
+#include "pmm.h"
 
 //! format of a memory region
 struct memory_region {
@@ -39,17 +40,17 @@ int _cdecl main(multiboot_info* info) {
 	setvect(19, (void(__cdecl &)(void))simd_fpu_fault);
 
 	interrupt_enable(); /*Enabling interrupt*/
-
 	DebugClrScreen(0x1f);
 	DebugSetColor(0x78);
 	DebugPuts("    Mango  Operating System is Loading ....                                     ");
 	DebugSetColor(0x19);
 	DebugPuts("we are optimizing th operating system WindowsX!\n");
 
-	unsigned int ram_amount = 1024;
-	ram_amount += info->m_memoryLo;
-	ram_amount += info->m_memoryHi * 64;
-	DebugPrintf("ram Amount : %d", ram_amount);
+	unsigned int memory_amount = 1024;
+	memory_amount += info->m_memoryLo;
+	memory_amount += info->m_memoryHi * 64;
+	DebugPrintf("ram Amount : %d", memory_amount);
+	pmm_initialize(memory_amount,0x100000+kernel_img_size*512);
 
 	DebugGotoXY(0, 24);
 	DebugSetColor(0x78);
@@ -57,19 +58,47 @@ int _cdecl main(multiboot_info* info) {
 
 
 	memory_region *mem_mep = (memory_region*)0x1000;
-	DebugGotoXY(0, 13);
+	DebugGotoXY(0, 3);
 	for (int i = 0; i < 15; i++){
 		if (i>0 && mem_mep[i].startLow == 0)
 			break;
-		DebugPrintf("Base : 0x%x%x and limit 0x%x%x type: %d \n",
-			mem_mep[i].startHigh, mem_mep[i].startLow, mem_mep[i].sizeHigh, mem_mep[i].sizeLow,
+		DebugPrintf("Base Frame : %d  and limit Block %d type: %d \n",
+			mem_mep[i].startLow / 4096, mem_mep[i].sizeLow / 4096,
 			mem_mep[i].type);
+		if (mem_mep[i].type == 1)	/*We Can Use it region*/
+			 pmm_clr_region(mem_mep[i].startLow, mem_mep[i].sizeLow);
 	}
+	pmm_set_region(0x100000,kernel_img_size*512);
 	
 	DebugPrintf("Kernel Block : %d \n", kernel_img_size);
 
+
+	DebugPrintf("\npmm regions initialized: %i allocation blocks; used or reserved blocks: %i\nfree blocks: %i\n",
+		pmm_get_max_blocks(), pmm_get_used_block_count(), pmm_get_free_block_count());
+
+	//! allocating and deallocating memory examples...
+
+	DebugSetColor(0x12);
+
+	uint32_t* p = (uint32_t*)pmm_alloc_block();
+	DebugPrintf("\np allocated at frame : %d", uint32_t(p) / 4096);
+
+	uint32_t* p2 = (uint32_t*)pmm_alloc_block_s(2);
+	DebugPrintf("\nallocated 2 blocks for p2 at frame : %d", uint32_t(p2) / 4096);
+
+	uint32_t* p3 = (uint32_t*)pmm_alloc_block();
+	DebugPrintf("\np allocated at frame : %d", uint32_t(p3) / 4096);
+
+	pmm_free_block(p);
+	p = (uint32_t*)pmm_alloc_block();
+	DebugPrintf("\nUnallocated p to free block 1. p is reallocated to frame : %d", uint32_t(p) / 4096);
+
+	pmm_free_block(p);
+	pmm_free_block_s(p2, 2);
+
+
 	for (;;){
-		DebugGotoXY(0, 12);
+		DebugGotoXY(0, 23);
 		DebugPrintf("Count = %d", get_tick());
 	}
 
---
+++ SystemCore/SysCore/Kernel/pmm.cpp
@@ -0,0 +1,162 @@
+/* ALi Fallah (c) 97/10/05 */
+
+#include <stdint.h>
+#include "pmm.h"
+#include <string.h>
+
+#define PMM_BLOCKS_PER_BYTE 8
+#define PMM_BLOCK_SIZE 4096
+#define PMM_BLOCK_ALIGN PMM_BLOCK_SIZE
+
+/*	Static Variable	*/
+static uint32_t		_pmm_memory_size = 0;
+static	uint32_t	_pmm_used_blocks = 0;
+static	uint32_t	_pmm_max_blocks  = 0;
+static	uint32_t*	_pmm_memory_map  = 0;
+
+/* Private Function */
+inline void pmm_map_set_block(uint32_t frame);
+inline void pmm_map_clr_block(uint32_t frame);
+inline bool pmm_test(uint32_t frame);
+int pmm_first_free();
+int pmm_first_free_s(size_t size);
+
+/*Body of Functions !*/
+
+inline void pmm_map_set_block(uint32_t frame){
+	_pmm_memory_map[frame / 32] |= (1 << (frame % 32));
+}
+
+inline void pmm_map_clr_block(uint32_t frame){
+	_pmm_memory_map[frame / 32] &= ~(1 << (frame % 32));
+}
+
+inline bool pmm_test(uint32_t frame){
+	return _pmm_memory_map[frame / 32] & (1 << (frame % 32));
+}
+
+int pmm_first_free (){
+	for (uint32_t i = 0; i < pmm_get_max_blocks() / 32; i++)
+		if (_pmm_memory_map[i] != 0xFFFFFFFF){
+			for (uint32_t j = 0; j < 32; j++)
+				if (!(_pmm_memory_map[i] & (1 << j)))
+					return i * 32 + j;
+		}
+	return -1;
+}
+
+int pmm_first_free_s(size_t size){
+	uint32_t counter=0;
+	if (size == 0)
+		return -1;
+
+	if (size == 1)
+		return pmm_first_free();
+
+	for (uint32_t i = 0; i < pmm_get_max_blocks() / 32; i++)
+		if (_pmm_memory_map[i] != 0xFFFFFFFF){
+			for (uint32_t j = 0; j < 32; j++){
+				if (!(_pmm_memory_map[i] & (1 << j)))
+					counter++;
+				else counter = 0;
+				if (counter == size)
+					return i * 32 + (j - (size - 1)); /*i test this function in another IDE*/
+			}
+		}
+		else counter = 0;
+	return -1;
+}
+
+void pmm_initialize(uint32_t mem_size, physical_addr bitmap){
+	_pmm_memory_size = mem_size;
+	_pmm_max_blocks = _pmm_memory_size * 1024 / PMM_BLOCK_SIZE;
+	_pmm_used_blocks = _pmm_max_blocks;
+	_pmm_memory_map = (physical_addr*)bitmap;
+	/*Set All Bitmap to 1 - it means All Memory are using*/
+	memset(_pmm_memory_map, 0xff, _pmm_max_blocks / PMM_BLOCKS_PER_BYTE);
+}
+
+void pmm_set_region(uint32_t base,uint32_t limit){
+	uint32_t block = base / PMM_BLOCK_SIZE;
+	uint32_t size = limit / PMM_BLOCK_SIZE;
+	for (uint32_t i = 0; i<size; i++)
+		pmm_map_set_block(block + size);
+	_pmm_used_blocks += size;
+	/*why?*/
+	pmm_map_set_block(0);
+}
+
+void pmm_clr_region(uint32_t base, uint32_t limit){
+	uint32_t frame = base / PMM_BLOCK_SIZE;
+	uint32_t size = limit / PMM_BLOCK_SIZE;
+	for (uint32_t i = 0; i<size; i++)
+		pmm_map_clr_block(frame++);
+	_pmm_used_blocks -= size;
+	/*why?*/
+	pmm_map_set_block(0);
+}
+
+void* pmm_alloc_block(){
+	if (pmm_get_free_block_count() <= 0)
+		return 0;
+	int frame = pmm_first_free();
+	if (frame == -1)
+		return 0;
+	pmm_map_set_block(frame);
+	physical_addr addr = frame*PMM_BLOCK_SIZE;
+	_pmm_used_blocks++;
+	return (void*)addr;
+
+}
+
+void pmm_free_block(physical_addr *pa){
+	physical_addr addr = (physical_addr)pa;
+	int frame = addr / PMM_BLOCK_SIZE;
+	pmm_map_clr_block(frame);
+	_pmm_used_blocks--;
+
+}
+
+void* pmm_alloc_block_s(uint32_t count){
+
+	if (pmm_get_free_block_count() <= 0)
+		return 0;
+
+	int first_frame = pmm_first_free_s(count);
+
+	if (first_frame == -1)
+		return 0;
+
+	for (uint32_t i = 0; i < count; i++)
+		pmm_map_set_block(first_frame + i);
+
+	physical_addr addr = first_frame * PMM_BLOCK_SIZE;
+	_pmm_used_blocks += count;
+	return (void*)addr;
+
+}
+
+void pmm_free_block_s(physical_addr *pa,uint32_t count){
+
+	physical_addr addr = (physical_addr)pa;
+	int frame = addr / PMM_BLOCK_SIZE;
+
+	for (uint32_t i = 0; i < count; i++)
+		pmm_map_clr_block(frame + i);
+
+	_pmm_used_blocks -= count;
+
+}
+
+uint32_t pmm_get_memory_size(){
+	return _pmm_memory_size;
+}
+uint32_t pmm_get_max_blocks(){
+	return _pmm_max_blocks;
+}
+uint32_t pmm_get_free_block_count(){
+	return _pmm_max_blocks-_pmm_used_blocks;
+}
+uint32_t pmm_get_used_block_count(){
+	return _pmm_used_blocks;
+}
